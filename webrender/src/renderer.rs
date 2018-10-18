21d7e6d9ec3c06e174a26468e4f11d4cd4c4ccee
Attila Dusnoki <attila.dusnoki@h-lab.eu>
2018-10-18 11:01:43 +0200

    Set scissor rect after enabling scissor with gfx

--- webrender/src/renderer.rs
+++ webrender/src/renderer.rs
@@ -2679,6 +2679,8 @@ impl<B: hal::Backend> Renderer<B>
 
         if scissor_rect.is_some() {
             self.device.enable_scissor();
+            #[cfg(not(feature = "gleam"))]
+            self.device.set_scissor_rect(scissor_rect.unwrap());
         }
     }
 
